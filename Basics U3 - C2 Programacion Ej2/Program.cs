bc0519c69a99d87ac8a0a4d64236cff37e6c1256
Agneta <agnetagsu@yahoo.es>
2021-05-22 18:25:50 +0200

    Agregar archivos de proyecto.

---
+++ Basics U3 - C2 Programacion Ej2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Basics_U3___C2_Programacion_Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 99;
+            double A = 5.9;
+            char C = 'a';
+            int valorASCII = 'a';
+
+            Console.WriteLine("Variable N = "+N+"\n");
+            Console.WriteLine("Variable A = "+A+"\n");
+            Console.WriteLine("Variable C = "+C+"\n");
+            Console.WriteLine("{0}+{1}={2}\n", N,A,N+A);
+            Console.WriteLine("{0}-{1}={2}\n", A, N, A-N);
+            Console.WriteLine("Valor numérico del carácter {0} = {1}\n", C, valorASCII);
+        }
+    }
+}
